ff475014b1ef3fa82f44c40fd01d1c15bbd638c0
Andrey Tim <shpolsky@gmail.com>
2012-01-11 23:46:44 +0600

    NBASpider v0.001

---
+++ NBASpider/NBASpider/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using HtmlAgilityPack;
+
+namespace NBASpider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri;
+            HtmlWeb web = new HtmlWeb();
+            BRTableParser parser = new BRTableParser();
+            HtmlDocument doc;
+
+            uri = "http://www.basketball-reference.com/players/j/jamesle01.html";
+            doc = web.Load(uri, "GET");
+            Console.Write(parser.GetTableCSVData(doc.GetElementbyId("totals")));
+
+            uri = "http://www.basketball-reference.com/teams/BOS/2012.html";
+            doc = web.Load(uri, "GET");
+            Console.Write(parser.GetTableCSVData(doc.GetElementbyId("injury")));
+            Console.Write(parser.GetTableCSVData(doc.GetElementbyId("roster")));
+            Console.Write(parser.GetTableCSVData(doc.GetElementbyId("team")));
+            Console.Write(parser.GetTableCSVData(doc.GetElementbyId("totals")));
+            Console.Write(parser.GetTableCSVData(doc.GetElementbyId("per_game")));
+            Console.Write(parser.GetTableCSVData(doc.GetElementbyId("per_minute")));
+            Console.Write(parser.GetTableCSVData(doc.GetElementbyId("advanced")));
+
+            new CSVFileWriter("data.csv").Write(parser.GetTableCSVData(doc.GetElementbyId("advanced")));
+
+            Console.WriteLine("Press Enter to terminate...");
+            Console.ReadLine();
+        }
+    }
+}
